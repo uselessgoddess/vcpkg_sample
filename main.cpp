8596d84fa367b8843592c37f0724c2e71184d434
uselessgoddess <dodickgod@gmail.com>
2021-05-26 07:43:48 +0300

    Real workable

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <memory>
+
 #include <Platform.Delegates.h>
 
 int main()
 {
-    auto x = Platform::Delegates::CreateDelegate(std::function([]() {}));
+    auto x = Platform::Delegates::CreateDelegate(std::function([]() {return -1;}));
+    return x();
 }
